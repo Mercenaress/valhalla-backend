ac5d53fc300c0bd290e99f55584f72f1ca3652c3
Mercenaress <linden9707@gmail.com>
2023-11-28 15:47:09 +0100

    setup post function and time/date input

---
+++ functions/postOrders/index.js
@@ -0,0 +1,52 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { nanoid } from "nanoid";
+
+const dbClient = new DynamoDBClient({
+    region: process.env.AWS_REGION,
+});
+const docClient = DynamoDBDocumentClient.from(dbClient);
+
+exports.handler = async (event) => {
+
+    const { progress, items, priceTotal, orderComment, customerInfo } = JSON.parse(event.body);
+  
+    try {
+      const id = nanoid();
+      const currentTime = new Date();
+      const date = currentTime.getFullYear()+'-'+(currentTime.getMonth()+1)+'-'+currentTime.getDate();
+      const time = currentTime.getHours()+':'+currentTime.getMinutes();
+      const orderDate = date+' '+time;
+      const putCommand = new PutCommand({
+        TableName: "order-db",
+        Order: {
+            id: id,
+            orderDate: orderDate,
+            progress: progress,
+            items: items,
+            priceTotal: priceTotal,
+            orderComment: orderComment,
+            customerInfo: customerInfo
+        },
+      });
+      console.log(putCommand);
+      await docClient.send(putCommand);
+      return { 
+          statusCode: 200,
+          body: JSON.stringify(
+              {
+              message: "Success!",
+              },
+          )
+      };
+    } catch (err) {
+      return { 
+        statusCode: 500,
+        body: JSON.stringify(
+            {
+            message: "Epic failure",
+            },
+        )
+    };
+    };
+  };
